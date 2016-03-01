b218c358bdbdfa3e8ccef4ff2f60be1daf6f64c5
Lawrence Fei Pan <lawrence_pan@hot-shot.com>
2016-03-01 13:38:28 -0500

    add material

--- shared/container/App.js
+++ shared/container/App.js
@@ -1,6 +1,12 @@
 import React, { Component, PropTypes } from 'react';
 import { connect } from 'react-redux';
 
+import Input from '../Input.jsx';
+
+import getMuiTheme from 'material-ui/lib/styles/getMuiTheme';
+import themeDecorator from 'material-ui/lib/styles/theme-decorator';
+
+@themeDecorator(getMuiTheme(null, { userAgent: 'all' }))
 class App extends Component {
   constructor(props, context) {
     super(props, context);
@@ -9,6 +15,7 @@ class App extends Component {
   render() {
     return (
       <div>
+        <Input />
         { this.props.children }
       </div>
     );
